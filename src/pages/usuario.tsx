3901fd96c06da98667861afcead9aa10df3865ed
hugodieb <hugodieb.hd@gmail.com>
2023-05-31 20:13:29 -0300

    cadastro de usuario moc

--- src/pages/usuario.tsx
+++ src/pages/usuario.tsx
@@ -1,14 +1,21 @@
 import Cabecalho from "@/components/template/Cabecalho";
 import Conteudo from "@/components/template/Conteudo";
 import Pagina from "@/components/template/Pagina";
+import TituloPagina from "@/components/template/TituloPagina";
+import { IconForms } from "@tabler/icons-react";
+import usuario from "@/data/constants/usuarioFalso";
 
 export default function CadastroUsuario() {
   return (
     <Pagina>
        <Cabecalho />
        <Conteudo>
-        Usuario!!!
-       </Conteudo>
+          <TituloPagina
+             icone={<IconForms/>}
+             principal="Dados Cadastrais"
+             secundario={`Informações de ${usuario.email}`}            
+          />
+        </Conteudo>
     </Pagina>    
   )
 }
